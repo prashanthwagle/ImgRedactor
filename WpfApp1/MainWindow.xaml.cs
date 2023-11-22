706e90e691305f99ed52833211192c9b0ce18d61
Prashanth <prashanthwagle360@gmail.com>
2023-11-22 14:56:15 -0500

    added exception handlers to methods

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -74,14 +74,28 @@ namespace SprayPaintApp
 
         private void BtnLoadFromFile_Click(object sender, RoutedEventArgs e)
         {
-            OpenFileDialog openFileDialog = new OpenFileDialog();
-            if (openFileDialog.ShowDialog() == true)
+            try
             {
-                Uri fileUri = new Uri(openFileDialog.FileName);
-                BitmapImage newImage = new BitmapImage(fileUri);
-                CreateImageOnCanvas(newImage);
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+                if (openFileDialog.ShowDialog() == true)
+                {
+                    Uri fileUri = new Uri(openFileDialog.FileName);
+                    if (!File.Exists(fileUri.LocalPath))
+                    {
+                        MessageBox.Show("Selected file does not exist.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                        return;
+                    }
+
+                    BitmapImage newImage = new BitmapImage(fileUri);
+                    CreateImageOnCanvas(newImage);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error loading image: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
             }
         }
+
 
         private void BtnClearCanvas_Click(object sender, RoutedEventArgs e)
         {
@@ -131,77 +145,111 @@ namespace SprayPaintApp
 
         private void Canvas_MouseMove(object sender, MouseEventArgs e)
         {
-            if (currentAction == EditAction.Spray)
+            try
             {
-                if (e.LeftButton == MouseButtonState.Pressed)
+                if (currentAction == EditAction.Spray)
                 {
-                    SprayPaint(e.GetPosition(paintCanvas));
-                }
-            } else if (currentAction == EditAction.Eraser)
-            {
-                if (e.LeftButton == MouseButtonState.Pressed)
-                {
-                    EraseSprayPaint(e.GetPosition(paintCanvas));
-                }
-            }
-        }
-
-        private void SprayPaint(Point mousePosition)
-        {
-            double sprayRadius = BrushThickness;
-            int sprayDensity = (int)Math.Ceiling(sprayRadius * 2);
-            Random random = new Random();
-
-            for (int i = 0; i < sprayDensity; i++)
-            {
-                double angle = random.NextDouble() * 2 * Math.PI;
-                double radius = Math.Sqrt(random.NextDouble()) * sprayRadius;
-
-                double x = mousePosition.X + radius * Math.Cos(angle);
-                double y = mousePosition.Y + radius * Math.Sin(angle);
-
-                Ellipse ellipse = new Ellipse
-                {
-                    Width = 3,
-                    Height = 3,
-                    Fill = new SolidColorBrush(sprayPaintClr.Value),
-                    Margin = new Thickness(x, y, 0, 0)
-                };
-
-                paintCanvas.Children.Add(ellipse);
-            }
-        }
-
-        private void EraseSprayPaint(Point mousePosition)
-        {
-            double eraseRadius = 5.0;
-            List<UIElement> elementsToRemove = new List<UIElement>();
-
-            foreach (UIElement element in paintCanvas.Children)
-            {
-                if (element is Ellipse ellipse)
-                {
-                    Point ellipsePoint = element.TransformToAncestor(paintCanvas).Transform(new Point(0, 0));
-
-                    if (Math.Abs(ellipsePoint.X - mousePosition.X) < eraseRadius && Math.Abs(ellipsePoint.Y - mousePosition.Y) < eraseRadius)
+                    if (e.LeftButton == MouseButtonState.Pressed)
                     {
-                        elementsToRemove.Add(ellipse);
+                        SprayPaint(e.GetPosition(paintCanvas));
+                    }
+                }
+                else if (currentAction == EditAction.Eraser)
+                {
+                    if (e.LeftButton == MouseButtonState.Pressed)
+                    {
+                        EraseSprayPaint(e.GetPosition(paintCanvas));
                     }
                 }
             }
-
-            foreach (UIElement elementToRemove in elementsToRemove)
+            catch (Exception exception)
             {
-                paintCanvas.Children.Remove(elementToRemove);
+                MessageBox.Show($"Error in mouse move: {exception.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
             }
         }
+
+
+        private void SprayPaint(Point mousePosition)
+        {
+            try
+            {
+                if (sprayPaintClr == null)
+                {
+                    MessageBox.Show("Spray paint color is not set.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+
+                double sprayRadius = BrushThickness;
+                int sprayDensity = (int)Math.Ceiling(sprayRadius * 2);
+                Random random = new Random();
+
+                for (int i = 0; i < sprayDensity; i++)
+                {
+                    double angle = random.NextDouble() * 2 * Math.PI;
+                    double radius = Math.Sqrt(random.NextDouble()) * sprayRadius;
+
+                    double x = mousePosition.X + radius * Math.Cos(angle);
+                    double y = mousePosition.Y + radius * Math.Sin(angle);
+
+                    Ellipse ellipse = new Ellipse
+                    {
+                        Width = 3,
+                        Height = 3,
+                        Fill = new SolidColorBrush(sprayPaintClr.Value),
+                        Margin = new Thickness(x, y, 0, 0)
+                    };
+
+                    paintCanvas.Children.Add(ellipse);
+                }
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show($"Error while Spraying Paint: {exception.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+
+        private void EraseSprayPaint(Point mousePosition)
+        {
+            try
+            {
+                double eraseRadius = 5.0;
+                List<UIElement> elementsToRemove = new List<UIElement>();
+
+                foreach (UIElement element in paintCanvas.Children)
+                {
+                    if (element is Ellipse ellipse)
+                    {
+                        Point ellipsePoint = element.TransformToAncestor(paintCanvas).Transform(new Point(0, 0));
+
+                        if (Math.Abs(ellipsePoint.X - mousePosition.X) < eraseRadius && Math.Abs(ellipsePoint.Y - mousePosition.Y) < eraseRadius)
+                        {
+                            elementsToRemove.Add(ellipse);
+                        }
+                    }
+                }
+
+                foreach (UIElement elementToRemove in elementsToRemove)
+                {
+                    paintCanvas.Children.Remove(elementToRemove);
+                }
+            }
+            catch (ArgumentNullException nullEx)
+            {
+                MessageBox.Show($"Error while Erasing Paint: {nullEx.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error while Erasing Paint: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
 
 
 
         private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
             BrushThickness = mySlider.Value;
-            SetEditAction(EditAction.Spray);
         }
 
         private void SprayClrPickerChanged(object sender, RoutedPropertyChangedEventArgs<Color?> e)
@@ -212,54 +260,85 @@ namespace SprayPaintApp
 
         private void SaveButton_Click(object sender, RoutedEventArgs e)
         {
-            SaveFileDialog saveFileDialog = new SaveFileDialog
+            try
             {
-                Filter = "PNG Image|*.png|JPEG Image|*.jpg;*.jpeg|Bitmap Image|*.bmp|All Files|*.*"
-            };
+                SaveFileDialog saveFileDialog = new SaveFileDialog
+                {
+                    Filter = "PNG Image|*.png|JPEG Image|*.jpg;*.jpeg|All Files|*.*"
+                };
 
-            if (saveFileDialog.ShowDialog() == true)
+                if (saveFileDialog.ShowDialog() == true)
+                {
+                    SaveImage(saveFileDialog.FileName);
+                }
+            }
+            catch (ArgumentNullException nullException)
             {
-                SaveImage(saveFileDialog.FileName);
+                MessageBox.Show($"Error while saving project: {nullException.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            catch (UnauthorizedAccessException unauthorizedException)
+            {
+                MessageBox.Show($"Error while saving project: {unauthorizedException.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show($"Error while saving project: {exception.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
             }
         }
+
 
         private void SaveImage(string filePath)
         {
-            RenderTargetBitmap renderTargetBitmap = new RenderTargetBitmap(
-                (int)paintCanvas.ActualWidth,
-                (int)paintCanvas.ActualHeight,
-                96,
-                96,
-                PixelFormats.Default);
-
-            renderTargetBitmap.Render(paintCanvas);
-
-            BitmapEncoder? encoder = null;
-            string fileExtension = System.IO.Path.GetExtension(filePath).ToLower();
-
-            switch (fileExtension)
+            try
             {
-                case ".png":
-                    encoder = new PngBitmapEncoder();
-                    break;
-                case ".jpg":
-                case ".jpeg":
-                    encoder = new JpegBitmapEncoder();
-                    break;
-                default:
-                    MessageBox.Show("Unsupported file format.");
-                    return;
+                RenderTargetBitmap renderTargetBitmap = new RenderTargetBitmap(
+                    (int)paintCanvas.ActualWidth,
+                    (int)paintCanvas.ActualHeight,
+                    96,
+                    96,
+                    PixelFormats.Default);
+
+                renderTargetBitmap.Render(paintCanvas);
+
+                BitmapEncoder? encoder = null;
+                string fileExtension = System.IO.Path.GetExtension(filePath).ToLower();
+
+                switch (fileExtension)
+                {
+                    case ".png":
+                        encoder = new PngBitmapEncoder();
+                        break;
+                    case ".jpg":
+                    case ".jpeg":
+                        encoder = new JpegBitmapEncoder();
+                        break;
+                    default:
+                        throw new NotSupportedException("Unsupported file format.");
+                }
+
+                encoder.Frames.Add(BitmapFrame.Create(renderTargetBitmap));
+
+                using (FileStream fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    encoder.Save(fileStream);
+                }
+
+                MessageBox.Show($"Saved Successfully to {filePath}");
             }
-
-            encoder.Frames.Add(BitmapFrame.Create(renderTargetBitmap));
-
-            using (FileStream fileStream = new FileStream(filePath, FileMode.Create))
+            catch (UnauthorizedAccessException unauthorizedException)
             {
-                encoder.Save(fileStream);
+                MessageBox.Show($"Error while saving image: {unauthorizedException.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
             }
-
-            MessageBox.Show($"Saved Successfully to {filePath}");
+            catch (NotSupportedException notSupportedException)
+            {
+                MessageBox.Show($"Error while saving image: {notSupportedException.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show($"Error while saving image: {exception.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
+
 
         private void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
@@ -268,10 +347,11 @@ namespace SprayPaintApp
 
         private void SaveAppState()
         {
+            
             if (paintCanvas != null)
             {
-                string dirPath = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "finalcover_llc");
-                string FILENAME = "$temp$.xaml";
+                string dirPath = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), AppDataFolder);
+                string FILENAME = FileName;
                 string hiddenFilePath = System.IO.Path.Combine(dirPath, FILENAME);
 
                 try
@@ -282,12 +362,18 @@ namespace SprayPaintApp
                         XamlWriter.Save(paintCanvas, fs);
                     }
                 }
-                catch (Exception ex)
+                catch (UnauthorizedAccessException unauthorizedException)
                 {
-                    Console.WriteLine($"Error AutoSaving file: {ex.Message}");
+                    MessageBox.Show($"Error autosaving file: {unauthorizedException.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                catch (Exception exception)
+                {
+                    MessageBox.Show($"Error autosaving file: {exception.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
                 }
             }
+            
         }
+
 
 
         private void MainWindow_Loaded(object sender, RoutedEventArgs e)
@@ -298,42 +384,47 @@ namespace SprayPaintApp
 
         private void RestoreAppState()
         {
-            string dirPath = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "finalcover_llc");
-            string FILENAME = "$temp$.xaml";
+            string dirPath = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), AppDataFolder);
+            string FILENAME = FileName;
             string hiddenFilePath = System.IO.Path.Combine(dirPath, FILENAME);
             List<UIElement> restoredElements = new List<UIElement>();
 
             try
             {
                 if (File.Exists(hiddenFilePath))
-
-                    Console.WriteLine(loadedCanvas.Children);
-
-                    if (loadedCanvas != null)
+                {
+                    using (FileStream fs = new FileStream(hiddenFilePath, FileMode.Open, FileAccess.Read))
                     {
+                        Canvas loadedCanvas = XamlReader.Load(fs) as Canvas;
 
-                        foreach (UIElement child in loadedCanvas.Children)
+                        if (loadedCanvas != null)
                         {
-                            restoredElements.Add(child);
-                  
+                            foreach (UIElement child in loadedCanvas.Children)
+                            {
+                                restoredElements.Add(child);
+                            }
+
+                            foreach (UIElement child in restoredElements)
+                            {
+                                loadedCanvas.Children.Remove(child);
+                                paintCanvas.Children.Add(child);
+                            }
                         }
-
-                        foreach (UIElement child in restoredElements)
-                        {
-                            loadedCanvas.Children.Remove(child);
-
-                            paintCanvas.Children.Add(child);
-                        }
-
                     }
                 }
-                
+                // If the file doesn't exist, simply skip the restoration
             }
-            catch (Exception ex)
+            catch (UnauthorizedAccessException unauthorizedException)
             {
-                Console.WriteLine($"Error Loading file: {ex.Message}");
+                MessageBox.Show($"Error autoloading previous state: {unauthorizedException.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show($"Error autoloading previous state: {exception.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
             }
         }
+
+
 
     }
 
